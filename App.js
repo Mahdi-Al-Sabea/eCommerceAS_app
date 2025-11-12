3dd8c88d27188709594f904a6d6183cb91f63866
Mahdi Al Sabea <mahdi.alsabea@gmail.com>
2025-11-12 18:36:37 +0200

    initialising App

---
+++ App.js
@@ -0,0 +1,13 @@
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaProvider>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+    </SafeAreaProvider>
+  );
+}
+
+
+
+export default App;
